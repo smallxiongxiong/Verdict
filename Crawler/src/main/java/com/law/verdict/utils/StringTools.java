2b7abfc0043b56ca20b6913fb87f2000fd53d7a7
smallxiongxiong <309233929@qq.com>
2018-04-01 16:53:15 +0800

    add

--- Crawler/src/main/java/com/law/verdict/utils/StringTools.java
+++ Crawler/src/main/java/com/law/verdict/utils/StringTools.java
@@ -1,8 +1,17 @@
 package com.law.verdict.utils;
 
 public class StringTools {
-		public static boolean isFailed(String responseStr){
-			if(responseStr.length()<1000)return true;
-			return false;
+//		public static boolean isResponseNULl(String responseStr){
+//			if(responseStr.startsWith("RF")){
+//				return false;
+//			}else{
+//				return true;
+//			}
+//		}
+		public static boolean isResponseOK(String code){
+			if(code.startsWith("RF")){
+				return false;
+			}
+			return true;
 		}
 }
