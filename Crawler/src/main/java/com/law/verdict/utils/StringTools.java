943b35d2e179ac4125309ce5bfa330f4c7d038e7
smallxiongxiong <309233929@qq.com>
2018-04-01 12:41:07 +0800

    add

--- Crawler/src/main/java/com/law/verdict/utils/StringTools.java
+++ Crawler/src/main/java/com/law/verdict/utils/StringTools.java
@@ -1,5 +1,8 @@
 package com.law.verdict.utils;
 
 public class StringTools {
-
+		public static boolean isFailed(String responseStr){
+			if(responseStr.length()<1000)return true;
+			return false;
+		}
 }
